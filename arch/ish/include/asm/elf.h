1ba463beca131c2162daa806c5b053eb3f730e16
Theodore Dubois <tblodt@icloud.com>
2020-08-03 13:45:53 -0700

    Clear out registers on exec

--- arch/ish/include/asm/elf.h
+++ arch/ish/include/asm/elf.h
@@ -1,7 +1,7 @@
 #ifndef __ASM_ISH_ELF_H
 #define __ASM_ISH_ELF_H
 
-/* TODO: don't hard code ABIs like this? it sucks? seriously this entire file sucks */
+/* TODO: don't hard code ABIs like this? it sucks? seriously this entire file sucks @abi */
 
 #define ELF_PLATFORM "i686"
 
@@ -18,6 +18,23 @@ typedef elf_greg_t elf_fpregset_t[ELF_NFPREG];
  */
 #define elf_check_arch(x) \
 	(((x)->e_machine == EM_386) || ((x)->e_machine == EM_486))
+
+/* SVR4/i386 ABI (pages 3-31, 3-32) says that when the program starts %edx
+   contains a pointer to a function which might be registered using `atexit'.
+   This provides a mean for the dynamic linker to call DT_FINI functions for
+   shared libraries that have been loaded before the code runs.
+
+   A value of 0 tells we have no such handler.
+
+   We might as well make sure everything else is cleared too (except for %esp),
+   just to make things more deterministic.
+ */
+#define ELF_PLAT_INIT(_r, load_addr)		\
+	do {					\
+	_r->bx = 0; _r->cx = 0; _r->dx = 0;	\
+	_r->si = 0; _r->di = 0; _r->bp = 0;	\
+	_r->ax = 0;				\
+} while (0)
 
 /*
  * These are used to set parameters in the core dumps.
