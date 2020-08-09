7f4ab2827b996e5e2ff1446515c64a35919fa9dc
Theodore Dubois <tblodt@icloud.com>
2020-08-08 19:41:14 -0700

    Add printk timestamps

--- arch/ish/kernel/syscalls_user.c
+++ arch/ish/kernel/syscalls_user.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <sys/mman.h>
+#include <time.h>
 
 ssize_t host_write(int fd, void *data, size_t len)
 {
@@ -9,4 +10,16 @@ ssize_t host_write(int fd, void *data, size_t len)
 void *host_mmap(void *addr, size_t len)
 {
 	return mmap(addr, len, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+}
+
+uint64_t host_monotonic_nanos(void)
+{
+#ifdef __APPLE__
+	return clock_gettime_nsec_np(CLOCK_UPTIME_RAW);
+#endif
+#ifdef __linux__
+	struct timespec ts;
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+	return ts.tv_sec * 1000000000 + ts.tv_nsec;
+#endif
 }
--- arch/ish/kernel/time.c
+++ arch/ish/kernel/time.c
@@ -1,5 +1,7 @@
 #include <linux/init.h>
+#include <linux/sched_clock.h>
 #include <asm/delay.h>
+#include <user/user.h>
 
 // TODO make these make sense
 void __const_udelay(unsigned long xloops)
@@ -16,4 +18,5 @@ void calibrate_delay(void)
 
 void __init time_init(void)
 {
+	sched_clock_register(host_monotonic_nanos, 64, 1000000000);
 }
