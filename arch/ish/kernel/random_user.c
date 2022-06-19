abe6c894964bef48f32d3b56340203d38acdf3bb
Theodore Dubois <tblodt@icloud.com>
2022-06-19 11:31:12 -0700

    Implement arch_get_random_int on linux

--- arch/ish/kernel/random_user.c
+++ arch/ish/kernel/random_user.c
@@ -1,10 +1,17 @@
-#include <stdlib.h>
 #include <stdbool.h>
+#if __linux__
+#include <sys/random.h>
+#elif __APPLE__
+#include <stdlib.h>
+#endif
 
 bool arch_get_random_int(unsigned int *v) {
 #if __APPLE__
 	*v = arc4random();
 	return true;
+#elif __linux__
+	int err = getrandom(v, sizeof(*v), 0);
+	return err > 0;
 #else
 	return false;
 #endif
