b4ac8d0096bcfd879ec090d35119f9c10db87813
Theodore Dubois <tblodt@icloud.com>
2022-08-28 14:41:16 -0700

    Update user.h definition of printk

--- arch/ish/include/user.h
+++ arch/ish/include/user.h
@@ -3,7 +3,8 @@
 
 extern void panic(const char *fmt, ...)
 	__attribute__ ((format (printf, 1, 2)));
-extern int printk(const char *fmt, ...)
+extern int _printk(const char *fmt, ...)
 	__attribute__ ((format (printf, 1, 2)));
+#define printk(...) _printk(__VA_ARGS__)
 
 #endif
